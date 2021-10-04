a4d161b0e2d48b0e8b9c2beeb9c1ba370dea708d
Guillaume Tucker <guillaume.tucker@collabora.com>
2021-10-04 21:14:17 +0100

    api.main: add handlers for things
    Add several URL handlers in api.main to create and retrieve Thing
    objects.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from .db import Database
+from .models import Thing
 
 app = FastAPI()
 db = Database()
@@ -8,3 +9,18 @@ db = Database()
 @app.get('/')
 async def root():
     return {"message": "KernelCI API"}
+
+
+@app.get('/thing/{thing_id}')
+def thing(thing_id: str):
+    return {'thing': db.find_by_id(Thing, thing_id)}
+
+
+@app.get('/things')
+def things():
+    return {'things': db.find_all(Thing)}
+
+
+@app.post('/thing')
+def create_thing(thing: Thing, token: str = Depends(get_current_user)):
+    return {'thing': db.create(thing)}
