23f1ef6d8bb7d829852c03156e36ce16bac0e91d
Guillaume Tucker <guillaume.tucker@collabora.com>
2021-10-05 21:39:19 +0100

    api: add OAuth2 user/password authentication
    Add api.auth with an Authentication class to provide a number of
    helper methods to use in the application, and use it in api.main to
    handle user/password and JWT authentication based on OAuth2.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

---
+++ api/auth.py
@@ -0,0 +1,66 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from typing import Optional
+from .models import User
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class Authentication:
+    # ToDo: use settings https://fastapi.tiangolo.com/advanced/settings/
+
+    # To generate a secret key: openssl rand -hex 32
+    SECRET_KEY = \
+        "fad13f9cf6e5ff51daac4e6784529daa9d1dc8a80703730221f1090066d929c4"
+
+    ALGORITHM = "HS256"
+
+    # Set to None so tokens don't expire
+    ACCESS_TOKEN_EXPIRE_MINUTES = None  # 30
+
+    def __init__(self, db):
+        self._pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+        self._db = db
+
+    def get_password_hash(self, password):
+        return self._pwd_context.hash(password)
+
+    def authenticate_user(self, username: str, password: str):
+        user = self._db.find_one(User, username=username)
+        if not user:
+            return False
+        if not self._pwd_context.verify(password, user.hashed_password):
+            return False
+        return user
+
+    def create_access_token(self, data: dict):
+        to_encode = data.copy()
+        if self.ACCESS_TOKEN_EXPIRE_MINUTES:
+            expires_delta = timedelta(minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES)
+            expire = datetime.utcnow() + expires
+            to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(
+            to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
+        return encoded_jwt
+
+    def get_current_user(self, token):
+        try:
+            payload = jwt.decode(
+                token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
+            username: str = payload.get("sub")
+            if username is None:
+                return None
+            token_data = TokenData(username=username)
+        except JWTError:
+            return None
+
+        return self._db.find_one(User, username=token_data.username)
--- api/main.py
+++ api/main.py
@@ -1,14 +1,59 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from .auth import Authentication, Token
 from .db import Database
-from .models import Thing
+from .models import Thing, User
 
 app = FastAPI()
 db = Database()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+auth = Authentication(db)
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = auth.get_current_user(token)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(user: User = Depends(get_current_user)):
+    if not user.active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return user
 
 
 @app.get('/')
 async def root():
     return {"message": "KernelCI API"}
+
+
+@app.post('/token', response_model=Token)
+async def login_for_access_token(
+        form_data: OAuth2PasswordRequestForm = Depends()):
+    user = auth.authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = auth.create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get('/me', response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+
+@app.get('/hash/{password}')
+def get_password_hash(password):
+    return auth.get_password_hash(password)
 
 
 @app.get('/thing/{thing_id}')
