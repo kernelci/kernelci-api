54f51945e5915a45348389f332c9b7d8008f8e28
Guillaume Tucker <guillaume.tucker@collabora.com>
2021-10-05 13:48:38 +0100

    api: add api.db.Database() abstraction class
    Add api.db with a Database() abstraction class so the FastAPI
    application can use it without communicating directly with the Mongo
    DB database.  This currently uses pymongo but it could be updated to
    use motor for asynchronous queries, in principle without changing the
    application code.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

---
+++ api/db.py
@@ -0,0 +1,45 @@
+from bson import ObjectId
+from pymongo import MongoClient
+from .models import Thing
+
+# ToDo: use motor
+# https://motor.readthedocs.io/en/stable/
+
+
+class Database(object):
+    COLLECTIONS = {
+        Thing: 'thing',
+    }
+
+    def __init__(self, host='db', db_name='kernelci'):
+        self._mongo = MongoClient(host=host)
+        self._db = self._mongo[db_name]
+
+    def _get_collection(self, model):
+        col = self.COLLECTIONS[model]
+        return self._db[col]
+
+    def find_all(self, model):
+        col = self._get_collection(model)
+        objs = []
+        for obj in col.find():
+            objs.append(model(**obj))
+        return objs
+
+    def find_one(self, model, **kwargs):
+        col = self._get_collection(model)
+        obj = col.find_one(kwargs)
+        return model(**obj) if obj else None
+
+    def find_by_id(self, model, obj_id):
+        col = self._get_collection(model)
+        obj = col.find_one({'_id': ObjectId(obj_id)})
+        return model(**obj) if obj else None
+
+    def create(self, obj):
+        col = self._get_collection(obj.__class__)
+        if hasattr(obj, 'id'):
+            delattr(obj, 'id')
+        res = col.insert_one(obj.dict(by_alias=True))
+        obj.id = res.inserted_id
+        return obj
--- api/main.py
+++ api/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from .db import Database
 
 app = FastAPI()
+db = Database()
+
 
 @app.get('/')
 async def root():
