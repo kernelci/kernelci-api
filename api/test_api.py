f99214d33fbcd48288ab41af5be4674885255284
Jeny Sadadia <jeny.sadadia@gmail.com>
2021-12-19 15:36:01 +0530

    API unit test case for /token endpoint
    api/test_api.py: Add test case for token endpoint
    docker/api/requirements-dev.txt: Add pytest-mock
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@gmail.com>

--- api/test_api.py
+++ api/test_api.py
@@ -5,7 +5,9 @@
 
 import pytest
 from .main import app
+from .models import User
 from fastapi.testclient import TestClient
+from unittest.mock import AsyncMock
 
 
 @pytest.fixture
@@ -23,3 +25,37 @@ def test_root_endpoint(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "KernelCI API"}
+
+
+@pytest.fixture()
+def mock_db_find_one(mocker):
+    async_mock = AsyncMock()
+    mocker.patch('api.db.Database.find_one',
+                 side_effect=async_mock)
+    return async_mock
+
+
+def test_token_endpoint(mock_db_find_one):
+    """
+    Test Case : Test KernelCI API token endpoint
+    Expected Result :
+        HTTP Response Code 200 OK
+        JSON with 'access_token' and 'token_type' key
+    """
+    user = User(username='bob',
+                hashed_password='$2b$12$CpJZx5ooxM11bCFXT76/z.o6HWs2sPJy4iP8.'
+                                'xCZGmM8jWXUXJZ4K',
+                active=True)
+    mock_db_find_one.return_value = user
+    client = TestClient(app)
+    response = client.post(
+        "/token",
+        headers={
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data={'username': 'bob', 'password': 'hello'}
+    )
+    print("json", response.json())
+    assert response.status_code == 200
+    assert 'access_token' and 'token_type' in response.json()
