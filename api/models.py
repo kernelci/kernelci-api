878128d12235c327e2a6c3b9423b501bb942d897
Guillaume Tucker <guillaume.tucker@collabora.com>
2021-12-20 21:44:22 +0000

    api.models: fix all Pylint errors
    Fix all Pylint errors in api.models except R0903 about the number of
    public methods in classes as most model classes don't have any methods
    at all.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- api/models.py
+++ api/models.py
@@ -3,12 +3,16 @@
 # Copyright (C) 2021 Collabora Limited
 # Author: Guillaume Tucker <guillaume.tucker@collabora.com>
 
+"""KernelCI API model definitions"""
+
+from typing import Optional
+
 from bson import ObjectId
 from pydantic import BaseModel, Field
-from typing import Optional
 
 
 class PyObjectId(ObjectId):
+    """Wrapper around ObjectId to be able to use it in Pydantic models"""
 
     @classmethod
     def __get_validators__(cls):
@@ -19,16 +23,25 @@ class PyObjectId(ObjectId):
         field_schema.update(type='string')
 
     @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
+    def validate(cls, value):
+        """Validate the value of the ObjectId"""
+        if not ObjectId.is_valid(value):
             raise ValueError('Invalid ObjectId')
-        return ObjectId(v)
+        return ObjectId(value)
 
 
 class ModelId(BaseModel):
+    """Pydantic model including a .id attribute for the Mongo DB _id
+
+    This Pydantic model class is a thin wrapper around `pydantic.BaseModel`
+    with an added `.id` attribute which then gets translated to the `_id`
+    attribute in Mongo DB documents using the `PyObjectId` class.
+    """
+
     id: Optional[PyObjectId] = Field(alias='_id')
 
     class Config:
+        """Configuration attributes for ModelId"""
         arbitrary_types_allowed = True
         json_encoders = {
             ObjectId: str,
@@ -40,17 +53,20 @@ class ModelId(BaseModel):
 #
 
 class User(ModelId):
+    """API user model"""
     username: str
     hashed_password: str
     active: bool
 
 
 class Thing(ModelId):
+    """Thing object model for early experiments with Fast API and Mongo DB"""
     name: str
     value: int
 
 
 class Revision(BaseModel):
+    """Linux kernel Git revision model"""
     tree: str
     url: str
     branch: str
@@ -59,6 +75,7 @@ class Revision(BaseModel):
 
 
 class Node(ModelId):
+    """KernelCI primitive node object model for generic test results"""
     kind: str = 'node'
     name: str
     revision: Revision
