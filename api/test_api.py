a0fa7c5f46bca007f50ace2b12664e39071d23c1
Jeny Sadadia <jeny.sadadia@gmail.com>
2021-12-21 10:47:13 +0530

    api/test_api.py: Add test case for /me endpoint
    Signed-off-by: Jeny Sadadia <jeny.sadadia@gmail.com>

--- api/test_api.py
+++ api/test_api.py
@@ -59,3 +59,40 @@ def test_token_endpoint(mock_db_find_one):
     print("json", response.json())
     assert response.status_code == 200
     assert 'access_token' and 'token_type' in response.json()
+
+
+@pytest.fixture()
+def mock_get_current_user(mocker):
+    async_mock = AsyncMock()
+    mocker.patch('api.auth.Authentication.get_current_user',
+                 side_effect=async_mock)
+    return async_mock
+
+
+def test_me_endpoint(mock_get_current_user):
+    """
+    Test Case : Test KernelCI API /me endpoint
+    Expected Result :
+        HTTP Response Code 200 OK
+        JSON with '_id', 'username', 'hashed_password'
+        and 'active' keys
+    """
+    user = User(username='bob',
+                hashed_password='$2b$12$CpJZx5ooxM11bCFXT76/z.o6HWs2sPJy4iP8.'
+                                'xCZGmM8jWXUXJZ4K',
+                active=True)
+    mock_get_current_user.return_value = user
+    client = TestClient(app)
+    response = client.get(
+        "/me",
+        headers={
+            "Accept": "application/json",
+            "Authorization": "Bearer "
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+            "eyJzdWIiOiJib2IifQ.ci1smeJeuX779PptTkuaG1S"
+            "Edkp5M1S1AgYvX8VdB20"
+        },
+    )
+    assert response.status_code == 200
+    assert ('_id' and 'username' and 'hashed_password' and
+            'active') in response.json()
