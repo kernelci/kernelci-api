172ee8ee116ce0119825b288bb656baaa0fd9f73
Jeny Sadadia <jeny.sadadia@gmail.com>
2022-01-06 12:02:03 +0530

    api/test_api.py: Add test_unsubscribe_endpoint_empty_response function
    Test case for /unsubscribe endpoint for the negative path
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@gmail.com>

--- api/test_api.py
+++ api/test_api.py
@@ -211,3 +211,32 @@ def test_unsubscribe_endpoint(mock_get_current_user,
             },
         )
         assert response.status_code == 200
+
+
+def test_unsubscribe_endpoint_empty_response(mock_get_current_user,
+                                             mock_init_sub_id):
+    """
+    Test Case : Test KernelCI API /unsubscribe endpoint negative path
+    Expected Result :
+        HTTP Response Code 204 HTTP_204_NO_CONTENT
+        JSON with 'detail' key
+    """
+    user = User(username='bob',
+                hashed_password='$2b$12$CpJZx5ooxM11bCFXT76/z.o6HWs2sPJy4iP8.'
+                                'xCZGmM8jWXUXJZ4K',
+                active=True)
+    mock_get_current_user.return_value = user
+
+    with TestClient(app) as client:
+        response = client.post(
+            "/unsubscribe/1",
+            headers={
+                "Authorization": "Bearer "
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+                "eyJzdWIiOiJib2IifQ.ci1smeJeuX779PptTkuaG1S"
+                "Edkp5M1S1AgYvX8VdB20"
+            },
+        )
+        print("response.json()", response.json())
+        assert response.status_code == 204
+        assert 'detail' in response.json()
