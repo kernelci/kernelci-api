a763bd86936d0eef1c22b93d4986352a0b94ae98
Jeny Sadadia <jeny.sadadia@gmail.com>
2022-01-06 11:59:34 +0530

    api/test_api.py: Add test_unsubscribe_endpoint function
    Test case for /unsubscribe endpoint for the positive path
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@gmail.com>

--- api/test_api.py
+++ api/test_api.py
@@ -145,6 +145,14 @@ def mock_subscribe(mocker):
     return async_mock
 
 
+@pytest.fixture()
+def mock_unsubscribe(mocker):
+    async_mock = AsyncMock()
+    mocker.patch('api.pubsub.PubSub.unsubscribe',
+                 side_effect=async_mock)
+    return async_mock
+
+
 def test_subscribe_endpoint(mock_get_current_user, mock_init_sub_id,
                             mock_subscribe):
     """
@@ -176,3 +184,30 @@ def test_subscribe_endpoint(mock_get_current_user, mock_init_sub_id,
         print("response.json()", response.json())
         assert response.status_code == 200
         assert ('id' and 'channel') in response.json()
+
+
+def test_unsubscribe_endpoint(mock_get_current_user,
+                              mock_init_sub_id, mock_unsubscribe):
+    """
+    Test Case : Test KernelCI API /unsubscribe endpoint positive path
+    Expected Result :
+        HTTP Response Code 200 OK
+    """
+    user = User(username='bob',
+                hashed_password='$2b$12$CpJZx5ooxM11bCFXT76/z.o6HWs2sPJy4iP8.'
+                                'xCZGmM8jWXUXJZ4K',
+                active=True)
+    mock_get_current_user.return_value = user
+    mock_unsubscribe.return_value = True
+
+    with TestClient(app) as client:
+        response = client.post(
+            "/unsubscribe/1",
+            headers={
+                "Authorization": "Bearer "
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+                "eyJzdWIiOiJib2IifQ.ci1smeJeuX779PptTkuaG1S"
+                "Edkp5M1S1AgYvX8VdB20"
+            },
+        )
+        assert response.status_code == 200
