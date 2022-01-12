0596ea5403c02aa1e5c36de589efbc5696cc8f95
Guillaume Tucker <guillaume.tucker@collabora.com>
2022-01-12 20:09:38 +0000

    api.db: fix all pylint errors
    Fix all pylint errors in api.db:
    
    * add missing docstrings
    * fix order of imports
    * drop unused imports
    * use new class declaration with implicit object inheritance
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- api/db.py
+++ api/db.py
@@ -3,13 +3,21 @@
 # Copyright (C) 2021 Collabora Limited
 # Author: Guillaume Tucker <guillaume.tucker@collabora.com>
 
+"""Database abstraction"""
+
 from bson import ObjectId
-from pymongo import MongoClient
-from .models import Node, User
 from motor import motor_asyncio
+from .models import Node, User
 
 
-class Database(object):
+class Database:
+    """Database abstraction class
+
+    This class provides an abstraction layer to access the Mongo DB database
+    asynchronously using the models defined in `.models`.  *host* is the
+    hostname where the database is, and *db_name* is the name of the database.
+    """
+
     COLLECTIONS = {
         User: 'user',
         Node: 'node',
@@ -24,6 +32,7 @@ class Database(object):
         return self._db[col]
 
     async def find_all(self, model):
+        """Find all objects of a given model"""
         col = self._get_collection(model)
         objs = []
         data = col.find()
@@ -32,21 +41,39 @@ class Database(object):
         return objs
 
     async def find_one(self, model, **kwargs):
+        """Find one object with matching attributes
+
+        The kwargs dictionary provides key/value pairs used to find an object
+        with matching attributes.
+        """
         col = self._get_collection(model)
         obj = await col.find_one(kwargs)
         return model(**obj) if obj else None
 
     async def find_by_id(self, model, obj_id):
+        """Find one object with a given id"""
         col = self._get_collection(model)
         obj = await col.find_one({'_id': ObjectId(obj_id)})
         return model(**obj) if obj else None
 
     async def find_by_attributes(self, model, attributes):
+        """Find objects with matching attributes
+
+        Find all objects with attributes matching the key/value pairs in the
+        attributes dictionary and return them as a list.
+        """
         col = self._get_collection(model)
         data = await col.find(attributes).to_list(None)
         return list(model(**obj) for obj in data)
 
     async def create(self, obj):
+        """Create a database document from a model object
+
+        For a given object instance from .models, create a document in the
+        database.  Add the new id to the object and return it.  Raise
+        ValueError if the object already has an id as this should be created by
+        the database.
+        """
         if obj.id is not None:
             raise ValueError(f"Object cannot be created with id: {obj.id}")
         delattr(obj, 'id')
@@ -56,6 +83,13 @@ class Database(object):
         return obj
 
     async def update(self, obj):
+        """Update an existing document from a model object
+
+        For a given object instance from .models, update the document in the
+        database with a matching id.  The object is then returned.  Raise a
+        ValueError if the object has no id or if no document matches the id in
+        the database.
+        """
         if obj.id is None:
             raise ValueError("Cannot update object with no id")
         col = self._get_collection(obj.__class__)
