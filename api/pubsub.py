kernelci/kernelci-api

commit 595285dab169c1ec3d68a78d47118316cf0db1e9

api.pubsub: use unique subscription IDs
Create a separate pubsub object for each subscription and associate it
with a unique ID stored in Redis.  This is to separate each client's
subscription, so each of them will receive all the events rather than
have only one pubsub object per user / channel combination.

Update api.main accordingly, with an asynchronous function to
initialise the PubSub object (it creates the ID counter in Redis which
is async).

Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

api/pubsub.py
@@ -14,6 +14,13 @@ class Settings(BaseSettings):
 
 
 class PubSub:
+    ID_KEY = 'kernelci-api-pubsub-id'
+
+    @classmethod
+    async def create(self, *args, **kwargs):
+        pubsub = PubSub(*args, **kwargs)
+        await pubsub._init_sub_id()
+        return pubsub
 
     def __init__(self, host='redis', db=1):
         self._redis = aioredis.from_url(f'redis://{host}/{db}')
@@ -21,30 +28,29 @@ class PubSub:
         self._lock = asyncio.Lock()
         self._settings = Settings()
 
-    async def subscribe(self, user, channel):
-        async with self._lock:
-            key = (user.id, channel)
-            if key not in self._subscriptions:
-                sub = self._redis.pubsub()
-                self._subscriptions[key] = sub
-                await sub.subscribe(channel)
-                return True
-            return False
+    async def _init_sub_id(self):
+        await self._redis.setnx(self.ID_KEY, 0)
 
-    async def unsubscribe(self, user, channel):
+    async def subscribe(self, channel):
+        sub_id = await self._redis.incr(self.ID_KEY)
         async with self._lock:
-            key = (user.id, channel)
-            sub = self._subscriptions.get(key)
+            sub = self._redis.pubsub()
+            self._subscriptions[sub_id] = sub
+            await sub.subscribe(channel)
+            return sub_id
+
+    async def unsubscribe(self, sub_id):
+        async with self._lock:
+            sub = self._subscriptions.get(sub_id)
             if sub:
-                self._subscriptions.pop(key)
-                sub.unsubscribe(channel)
+                self._subscriptions.pop(sub_id)
+                sub.unsubscribe()
                 return True
             return False
 
-    async def listen(self, user, channel):
+    async def listen(self, sub_id):
         async with self._lock:
-            key = (user.id, channel)
-            sub = self._subscriptions.get(key)
+            sub = self._subscriptions.get(sub_id)
             if not sub:
                 return None
 

api/main.py
@@ -14,7 +14,13 @@ app = FastAPI()
 db = Database()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 auth = Authentication(db)
-pubsub = PubSub()
+pubsub = None
+
+
+@app.on_event('startup')
+async def pubsub_startup():
+    global pubsub
+    pubsub = await PubSub.create()
 
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
@@ -86,27 +92,22 @@ async def create_thing(thing: Thing, token: str = Depends(get_current_user)):
 
 @app.post('/subscribe/{channel}')
 async def subscribe(channel: str, user: User = Depends(get_user)):
-    res = await pubsub.subscribe(user, channel)
+    return await pubsub.subscribe(channel)
+
+
+@app.post('/unsubscribe/{sub_id}')
+async def unsubscribe(sub_id: int, user: User = Depends(get_user)):
+    res = await pubsub.unsubscribe(sub_id)
     if res is False:
         raise HTTPException(
             status_code=status.HTTP_204_NO_CONTENT,
-            detail=f"Already subscribed to channel: {channel}"
+            detail=f"Already unsubscribed: {sub_id}"
         )
 
 
-@app.post('/unsubscribe/{channel}')
-async def unsubscribe(channel: str, user: User = Depends(get_user)):
-    res = await pubsub.unsubscribe(user, channel)
-    if res is False:
-        raise HTTPException(
-            status_code=status.HTTP_204_NO_CONTENT,
-            detail=f"Already unsubscribed from channel: {channel}"
-        )
-
-
-@app.get('/listen/{channel}')
-async def listen(channel: str, user: User = Depends(get_user)):
-    msg = await pubsub.listen(user, channel)
+@app.get('/listen/{sub_id}')
+async def listen(sub_id: int, user: User = Depends(get_user)):
+    msg = await pubsub.listen(sub_id)
     if msg is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,

commit ee15edeb0fd70bd97ae4c6599a0e5642922e809e

api/pubsub.py: use 'await' with aioredis unsubscribe() call
Fix warning:
api/pubsub.py:52: RuntimeWarning: coroutine 'PubSub.execute_command'
was never awaited :sub.unsubscribe()

Fixes: 595285d ("api.pubsub: use unique subscription IDs")
Signed-off-by: Jeny Sadadia <jeny.sadadia@gmail.com>

api/pubsub.py
@@ -49,7 +49,7 @@ class PubSub:
             sub = self._subscriptions.get(sub_id)
             if sub:
                 self._subscriptions.pop(sub_id)
-                sub.unsubscribe()
+                await sub.unsubscribe()
                 return True
             return False
 