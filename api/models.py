kernelci/kernelci-api

commit 15380acb93c292d7fe049cfc56cc2f7843b18956

api.models: add Revision.url and .describe
Add .url and .describe fields to the Revision model, to contain the
Git URL and Git describe strings.  This should help align with the
current bmeta.json schema used by kernelci-core.

Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

api/models.py
@@ -52,8 +52,10 @@ class Thing(ModelId):
 
 class Revision(BaseModel):
     tree: str
+    url: str
     branch: str
     commit: str
+    describe: str
 
 
 class Node(ModelId):

commit a185002ae559bb4113732925db4680825bf43432

api/models.py: modify Revision.describe to be Optional
trigger script from the kernelci-pipeline will post
a new node with an empty Revision.describe property.
When a new revision is available, the client will
update the revision information with the git describe.

Fixes: 15380ac ("api.models: add Revision.url and .describe")
Signed-off-by: Jeny Sadadia <jeny.sadadia@gmail.com>

api/models.py
@@ -65,7 +65,7 @@ class Revision(BaseModel):
     url: str
     branch: str
     commit: str
-    describe: str
+    describe: Optional[str] = None
 
 
 class Node(ModelId):