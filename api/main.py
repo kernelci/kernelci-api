aebe8bcd41500129bf3a0b2001bb9b7b9c244b18
Guillaume Tucker <guillaume.tucker@collabora.com>
2022-04-27 16:29:50 +0100

    api.models: add Node.translate_fields() method
    Rewrite Node.replace_parent() method with a more generic one to
    translate all applicable fields to objects such as database IDs.
    Update how it's used in api.main accordingly to keep the code in
    related to the model's fields in api.models.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- api/main.py
+++ api/main.py
@@ -87,16 +87,15 @@ async def get_node(node_id: str):
 
 @app.get('/nodes', response_model=List[Node])
 async def get_nodes(request: Request):
-    is_valid, msg = Node.validate_params(dict(request.query_params))
+    query_params = dict(request.query_params)
+    is_valid, msg = Node.validate_params(query_params)
     if not is_valid:
         raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Invalid request parameters: {}".format(msg)
             )
-    query_param_dict = dict(request.query_params)
-    if 'parent' in query_param_dict:
-        query_param_dict = Node.modify_parent(query_param_dict)
-    return await db.find_by_attributes(Node, query_param_dict)
+    translated_params = Node.translate_fields(query_params)
+    return await db.find_by_attributes(Node, translated_params)
 
 
 @app.get('/get_root_node/{node_id}', response_model=Node)
