304db16ae00352f91291c05ff24d9d5b482c5e2c
Jeny Sadadia <jeny.sadadia@collabora.com>
2022-05-16 12:55:54 +0530

    test_token_handler.py: add test to get token for admin
    Unit test for /token endpoint to get token with
    admin scope for Admin user.
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

--- test/test_token_handler.py
+++ test/test_token_handler.py
@@ -2,6 +2,9 @@
 #
 # Copyright (C) 2022 Jeny Sadadia
 # Author: Jeny Sadadia <jeny.sadadia@gmail.com>
+#
+# Copyright (C) 2022 Collabora Limited
+# Author: Jeny Sadadia <jeny.sadadia@collabora.com>
 
 """Unit test function for KernelCI API token handler"""
 
@@ -65,3 +68,29 @@ def test_token_endpoint_incorrect_password(mock_db_find_one):
     print("response json", response.json())
     assert response.status_code == 401
     assert response.json() == {'detail': 'Incorrect username or password'}
+
+
+def test_token_endpoint_admin_user(mock_db_find_one):
+    """
+    Test Case : Test KernelCI API token endpoint for admin user
+    Expected Result :
+        HTTP Response Code 200 OK
+        JSON with 'access_token' and 'token_type' key
+    """
+    user = User(username='test_admin',
+                hashed_password='$2b$12$CpJZx5ooxM11bCFXT76/z.o6HWs2sPJy4iP8.'
+                                'xCZGmM8jWXUXJZ4K',
+                active=True, is_admin=True)
+    mock_db_find_one.return_value = user
+    client = TestClient(app)
+    response = client.post(
+        "/token",
+        headers={
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data={'username': 'test_admin', 'password': 'hello', 'scope': 'admin'}
+    )
+    print("json", response.json())
+    assert response.status_code == 200
+    assert ('access_token', 'token_type') == tuple(response.json().keys())
