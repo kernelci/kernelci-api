90da25ae00c0c459db2e156e4f806183840171df
Jeny Sadadia <jeny.sadadia@collabora.com>
2022-05-17 14:36:50 +0530

    _sample_settings.py: sample file for settings
    Provide settings for admin utility script.
    Add variable for bearer token to POST request
    to API.
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

---
+++ admin/_sample_settings.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Copyright (C) 2022 Collabora Limited
+# Author: Jeny Sadadia <jeny.sadadia@collabora.com>
+
+"""Sample settings file"""
+
+# Rename the file to _settings.py
+# Provide authorization bearer token
+
+# TOKEN =
