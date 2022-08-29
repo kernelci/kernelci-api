e2f3315b8f38e14bef78ef5b5ba813dba1fe3f83
Jeny Sadadia <jeny.sadadia@collabora.com>
2022-08-29 17:17:51 +0530

    api.paginator_models: add model to redefine limit
    Add 'CustomLimitOffsetParams' class to redefine
    limit parameter with custom validations.
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

--- api/paginator_models.py
+++ api/paginator_models.py
@@ -6,14 +6,26 @@
 """Model definitions for pagination"""
 
 from typing import TypeVar
-from fastapi_pagination import LimitOffsetPage
+from fastapi import Query
+from fastapi_pagination import LimitOffsetPage, LimitOffsetParams
 from bson import ObjectId
+
+
+class CustomLimitOffsetParams(LimitOffsetParams):
+    """Model to set custom constraint on limit
+
+    The model is required to redefine limit parameter to remove the number
+    validation on maximum value"""
+
+    limit: int = Query(50, ge=1, description="Page size limit")
 
 
 class PageModel(LimitOffsetPage[TypeVar("T")]):
     """Model for pagination
 
     This model is required to serialize paginated model data response"""
+
+    __params_type__ = CustomLimitOffsetParams
 
     class Config:
         """Configuration attributes for PageNode"""
