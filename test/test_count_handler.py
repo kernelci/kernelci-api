be73d036bc0bf846475d52c234541a665a6bea44
Jeny Sadadia <jeny.sadadia@collabora.com>
2022-09-09 15:44:29 +0530

    test_count_handler: add tests for '/count' endpoint
    Implement unit tests for '/count' endpoint.
    The endpoint can be used to get total number
    of node objects matching provided attributes.
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

---
+++ test/test_count_handler.py
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Copyright (C) 2022 Collabora Limited
+# Author: Jeny Sadadia <jeny.sadadia@collabora.com>
+
+# pylint: disable=unused-argument
+
+"""Unit test functions for KernelCI API count handler"""
+
+
+from fastapi.testclient import TestClient
+from api.main import app
+
+
+def test_count_nodes(mock_db_count, mock_init_sub_id):
+    """
+    Test Case : Test KernelCI API GET /count endpoint
+    Expected Result :
+        HTTP Response Code 200 OK
+        Total number of nodes available
+    """
+    mock_db_count.return_value = 10
+    with TestClient(app) as client:
+        response = client.get("/count")
+        print("response.json()", response.json())
+        assert response.status_code == 200
+        assert response.json() >= 0
+
+
+def test_count_nodes_matching_attributes(mock_db_count, mock_init_sub_id):
+    """
+    Test Case : Test KernelCI API GET /count endpoint with attributes
+    Expected Result :
+        HTTP Response Code 200 OK
+        Number of nodes matching attributes
+    """
+    mock_db_count.return_value = 1
+    with TestClient(app) as client:
+        response = client.get("/count?name=checkout")
+        print("response.json()", response.json())
+        assert response.status_code == 200
+        assert response.json() == 1
