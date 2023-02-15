8632137e9767a907f6c77be5a21395231df7a85b
Jeny Sadadia <jeny.sadadia@collabora.com>
2023-02-15 19:14:00 +0530

    e2e_tests: end to end test for subscribe handler
    Add a test module `test_subscribe_handler.py `for
    `/subscribe` endpoint.
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

---
+++ e2e_tests/test_subscribe_handler.py
@@ -0,0 +1,55 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Copyright (C) 2023 Collabora Limited
+# Author: Jeny Sadadia <jeny.sadadia@collabora.com>
+
+
+"""End-to-end test functions for KernelCI API subscribe handler"""
+
+import pytest
+
+
+@pytest.mark.dependency(
+    depends=['e2e_tests/test_user_creation.py::test_create_regular_user'],
+    scope='session')
+@pytest.mark.order(3)
+def test_subscribe_node_channel(test_client):
+    """
+    Test Case : Test KernelCI API '/subscribe' endpoint with 'node' channel
+    Expected Result :
+        HTTP Response Code 200 OK
+        JSON with subscription 'id' and 'channel' keys
+    """
+    response = test_client.post(
+        "subscribe/node",
+        headers={
+            "Authorization": f"Bearer {pytest.BEARER_TOKEN}"
+        },
+    )
+    pytest.node_channel_subscription_id = response.json()['id']
+    assert response.status_code == 200
+    assert ('id', 'channel') == tuple(response.json().keys())
+    assert response.json().get('channel') == 'node'
+
+
+@pytest.mark.dependency(
+    depends=['e2e_tests/test_user_creation.py::test_create_regular_user'],
+    scope='session')
+@pytest.mark.order(3)
+def test_subscribe_test_channel(test_client):
+    """
+    Test Case : Test KernelCI API '/subscribe' endpoint with 'test_channel'
+    Expected Result :
+        HTTP Response Code 200 OK
+        JSON with subscription 'id' and 'channel' keys
+    """
+    response = test_client.post(
+        "subscribe/test_channel",
+        headers={
+            "Authorization": f"Bearer {pytest.BEARER_TOKEN}"
+        },
+    )
+    pytest.test_channel_subscription_id = response.json()['id']
+    assert response.status_code == 200
+    assert ('id', 'channel') == tuple(response.json().keys())
+    assert response.json().get('channel') == 'test_channel'
