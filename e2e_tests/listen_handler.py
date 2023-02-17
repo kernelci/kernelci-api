6ed0ce860e1ea58222541cda301ee8e1944567dd
Jeny Sadadia <jeny.sadadia@collabora.com>
2023-02-17 12:45:06 +0530

    e2e_tests/listen_handler: Helper function for '/listen' handler
    Helper function to create and return an asyncio Task to listen
    to pubsub events on node channel using API endpoint `/listen`.
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

---
+++ e2e_tests/listen_handler.py
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Copyright (C) 2023 Collabora Limited
+# Author: Jeny Sadadia <jeny.sadadia@collabora.com>
+
+
+"""Helper function for KernelCI API listen handler"""
+
+import pytest
+import asyncio
+
+
+def create_listen_task(test_async_client, subscription_id):
+    """
+    Create an asyncio Task to listen to pubsub events on node channel using
+    API endpoint `/listen`.
+    Returns the task instance.
+    """
+    listen_path = '/'.join(['listen', str(subscription_id)])
+    task_listen = asyncio.create_task(
+        test_async_client.get(
+            listen_path,
+            headers={
+                "Accept": "application/json",
+                "Authorization": f"Bearer {pytest.BEARER_TOKEN}"
+            },
+        )
+    )
+    return task_listen
