db8401ca85e9a9368491097ad25d6bb9fdd00c92
Guillaume Tucker <guillaume.tucker@collabora.com>
2023-06-26 17:39:59 +0200

    api.admin: add tool to create admin user and group
    Add a Python module api.admin which can be run directly to bootstrap a
    new database with an admin user and group.  This uses the
    Authenticator, Database and model classes to ensure the data is
    created in a compatible way with the API application but doesn't
    require any services to be running (except MongoDB somewhere).
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

---
+++ api/admin.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Copyright (C) 2022, 2023 Collabora Limited
+# Author: Jeny Sadadia <jeny.sadadia@collabora.com>
+# Author: Michal Galka <michal.galka@collabora.com>
+# Author: Guillaume Tucker <guillaume.tucker@collabora.com>
+
+"""Command line utility for creating an admin user"""
+
+import asyncio
+import argparse
+import sys
+
+import getpass
+
+from .auth import Authentication
+from .db import Database
+from .models import User, UserGroup
+
+
+async def setup_admin_group(db):
+    group_obj = await db.find_one(UserGroup, name='admin')
+    if group_obj is None:
+        print("Creating admin group...")
+        group_obj = await db.create(UserGroup(name='admin'))
+    return group_obj
+
+
+async def setup_admin_user(db, username, admin_group):
+    user_obj = await db.find_one(User, username=username)
+    if user_obj:
+        print(f"User {username} already exists, aborting.")
+        print(user_obj.json())
+        return None
+    password = getpass.getpass(f"Password for user '{args.username}': ")
+    hashed_password = Authentication.get_password_hash(password)
+    print(f"Creating {username} user...")
+    return await db.create(User(
+        username=username,
+        hashed_password=hashed_password,
+        groups=[admin_group]
+    ))
+
+
+async def main(args):
+    db = Database(args.mongo, args.database)
+    group = await setup_admin_group(db)
+    user = await setup_admin_user(db, args.username, group)
+    return True
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Create KernelCI API admin user")
+    parser.add_argument('--mongo', default='mongodb://db:27017',
+                        help="Mongo server connection string")
+    parser.add_argument('--username', default='admin',
+                        help="Admin username")
+    parser.add_argument('--database', default='kernelci',
+                        help="KernelCI database name")
+    args = parser.parse_args()
+    sys.exit(0 if asyncio.run(main(args)) else 1)
