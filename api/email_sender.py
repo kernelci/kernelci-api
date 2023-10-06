5f4d2615262fbec91308c5793c607d3f2d31fb9e
Jeny Sadadia <jeny.sadadia@collabora.com>
2023-10-06 14:50:52 +0530

    api/user_manager.py: user management logic
    Add `user_manager.py` to implement custom user management
    logic. Extend `BaseUserManager` provided by `fastapi-users` to
    have customized core logic for user management.
    Add database adapter `get_user_db` to create
    a link between user model from `fastapi-users` and API
    database configuration.
    Inject `UserManager` at a runtime in a database session.
    Add `email_sender` module to send verification and
    reset password token for user accounts via email. Add
    email templates.
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

---
+++ api/email_sender.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Copyright (C) 2023 Jeny Sadadia
+# Author: Jeny Sadadia <jeny.sadadia@collabora.com>
+
+"""SMTP Email Sender module"""
+
+from email.mime.multipart import MIMEMultipart
+import email
+import email.mime.text
+import smtplib
+from pydantic import BaseSettings, EmailStr
+
+
+class Settings(BaseSettings):
+    """Email settings"""
+    smtp_host: str
+    smtp_port: int
+    email_sender: EmailStr
+    email_password: str
+
+
+class EmailSender:
+    """Class to send email report using SMTP"""
+    def __init__(self):
+        self._settings = Settings()
+
+    def _smtp_connect(self):
+        """Method to create a connection with SMTP server"""
+        if self._settings.smtp_port == 465:
+            smtp = smtplib.SMTP_SSL(self._settings.smtp_host,
+                                    self._settings.smtp_port)
+        else:
+            smtp = smtplib.SMTP(self._settings.smtp_host,
+                                self._settings.smtp_port)
+            smtp.starttls()
+        smtp.login(self._settings.email_sender,
+                   self._settings.email_password)
+        return smtp
+
+    def _create_email(self, email_subject, email_content, email_recipient):
+        """Method to create an email message from email subject, contect,
+        sender, and receiver"""
+        email_msg = MIMEMultipart()
+        email_text = email.mime.text.MIMEText(email_content, "plain", "utf-8")
+        email_text.replace_header('Content-Transfer-Encoding', 'quopri')
+        email_text.set_payload(email_content, 'utf-8')
+        email_msg.attach(email_text)
+        email_msg['To'] = email_recipient
+        email_msg['From'] = self._settings.email_sender
+        email_msg['Subject'] = email_subject
+        return email_msg
+
+    def _send_email(self, email_msg):
+        """Method to send an email message using SMTP"""
+        smtp = self._smtp_connect()
+        if smtp:
+            smtp.send_message(email_msg)
+            smtp.quit()
+
+    def create_and_send_email(self, email_subject, email_content,
+                              email_recipient):
+        """Method to create and send email"""
+        email_msg = self._create_email(
+            email_subject, email_content, email_recipient
+        )
+        self._send_email(email_msg)
---
+++ api/user_manager.py
@@ -0,0 +1,116 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Copyright (C) 2023 Collabora Limited
+# Author: Jeny Sadadia <jeny.sadadia@collabora.com>
+
+"""User Manager"""
+
+from typing import Optional, Any, Dict
+from fastapi_users import BaseUserManager
+from fastapi_users.db import (
+    BaseUserDatabase,
+    BeanieUserDatabase,
+    ObjectIDIDMixin,
+)
+from fastapi_users.password import PasswordHelperProtocol
+from beanie import PydanticObjectId
+from fastapi import Depends, Request
+import jinja2
+from .user_models import User
+from .auth import Settings
+from .email_sender import EmailSender
+
+
+class UserManager(ObjectIDIDMixin, BaseUserManager[User, PydanticObjectId]):
+    """User management logic"""
+    settings = Settings()
+    reset_password_token_secret = settings.secret_key
+    verification_token_secret = settings.secret_key
+
+    def __init__(self, user_db: BaseUserDatabase[User, PydanticObjectId],
+                 password_helper: PasswordHelperProtocol | None = None):
+        self._email_sender = EmailSender()
+        self._template_env = jinja2.Environment(
+                            loader=jinja2.FileSystemLoader(
+                                "./templates/")
+                        )
+        super().__init__(user_db, password_helper)
+
+    async def on_after_register(self, user: User,
+                                request: Optional[Request] = None):
+        """Handler to execute after successful user registration"""
+        print(f"User {user.id} has registered.")
+
+    async def on_after_request_verify(self, user: User, token: str,
+                                      request: Optional[Request] = None):
+        """Handler to execute after successful verification request"""
+        template = self._template_env.get_template("email-verification.jinja2")
+        subject = "Email verification Token for KernelCI API account"
+        content = template.render(
+            username=user.username, token=token
+        )
+        self._email_sender.create_and_send_email(subject, content, user.email)
+
+    async def on_after_verify(self, user: User,
+                              request: Optional[Request] = None):
+        """Handler to execute after successful user verification"""
+        print(f"Verification successful for user {user.id}")
+        template = self._template_env.get_template(
+            "email-verification-successful.jinja2")
+        subject = "Email verification successful for KernelCI API account"
+        content = template.render(
+            username=user.username,
+        )
+        self._email_sender.create_and_send_email(subject, content, user.email)
+
+    async def on_after_login(self, user: User,
+                             request: Optional[Request] = None):
+        """Handler to execute after successful user login"""
+        print(f"User {user.id} logged in.")
+
+    async def on_after_forgot_password(self, user: User, token: str,
+                                       request: Optional[Request] = None):
+        """Handler to execute after successful forgot password request"""
+        template = self._template_env.get_template("reset-password.jinja2")
+        subject = "Reset Password Token for KernelCI API account"
+        content = template.render(
+            username=user.username, token=token
+        )
+        self._email_sender.create_and_send_email(subject, content, user.email)
+
+    async def on_after_reset_password(self, user: User,
+                                      request: Optional[Request] = None):
+        """Handler to execute after successful password reset"""
+        print(f"User {user.id} has reset their password.")
+        template = self._template_env.get_template(
+            "reset-password-successful.jinja2")
+        subject = "Password reset successful for KernelCI API account"
+        content = template.render(
+            username=user.username,
+        )
+        self._email_sender.create_and_send_email(subject, content, user.email)
+
+    async def on_after_update(self, user: User, update_dict: Dict[str, Any],
+                              request: Optional[Request] = None):
+        """Handler to execute after successful user update"""
+        print(f"User {user.id} has been updated.")
+
+    async def on_before_delete(self, user: User,
+                               request: Optional[Request] = None):
+        """Handler to execute before user delete."""
+        print(f"User {user.id} is going to be deleted.")
+
+    async def on_after_delete(self, user: User,
+                              request: Optional[Request] = None):
+        """Handler to execute after user delete."""
+        print(f"User {user.id} is successfully deleted.")
+
+
+async def get_user_db():
+    """Database adapter for fastapi-users"""
+    yield BeanieUserDatabase(User)
+
+
+async def get_user_manager(user_db: BeanieUserDatabase = Depends(get_user_db)):
+    """Get user manager"""
+    yield UserManager(user_db)
