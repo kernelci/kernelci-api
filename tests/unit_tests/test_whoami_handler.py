56a429ffe6c9345bf4b6a7be211b67868089ec1a
Jeny Sadadia <jeny.sadadia@collabora.com>
2023-10-14 17:58:12 +0530

    tests/unit_tests: update user related tests
    Integrate `mongomock_motor` package to mock
    async mongo client for Beanie initialization.
    Add fixture to initialize Beanie on app startup.
    Remove fixtures and add methods to get current active user.
    Update `app.dependency_overrides` dictionary to mock
    dependency callable from `fastapi-users` to get users
    while creating `TestClient`.
    Update all the user related tests.
    Add environment variable to `pytest` command in Github
    actions workflow.
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

--- tests/unit_tests/test_whoami_handler.py
+++ tests/unit_tests/test_whoami_handler.py
@@ -10,10 +10,13 @@
 
 """Unit test function for KernelCI API whoami handler"""
 
+import pytest
 from tests.unit_tests.conftest import BEARER_TOKEN
+from api.user_models import UserRead
 
 
-def test_whoami_endpoint(mock_get_current_user, test_client):
+@pytest.mark.asyncio
+async def test_whoami_endpoint(test_async_client, mock_auth_current_user):
     """
     Test Case : Test KernelCI API /whoami endpoint
     Expected Result :
@@ -21,12 +24,25 @@ def test_whoami_endpoint(mock_get_current_user, test_client):
         JSON with 'id', 'username', 'hashed_password'
         and 'active' keys
     """
-    response = test_client.get(
+    user = UserRead(
+            id='61bda8f2eb1a63d2b7152420',
+            username='test-user',
+            email='test-user@kernelci.org',
+            groups=[],
+            is_active=True,
+            is_verified=False,
+            is_superuser=False
+        )
+    mock_auth_current_user.return_value = user, BEARER_TOKEN
+    response = await test_async_client.get(
         "whoami",
         headers={
             "Accept": "application/json",
             "Authorization": BEARER_TOKEN
         },
     )
+    print(response.json(), response.status_code)
     assert response.status_code == 200
-    assert ('id', 'active', 'profile') == tuple(response.json().keys())
+    assert ('id', 'email', 'is_active', 'is_superuser',
+            'is_verified', 'username',
+            'groups') == tuple(response.json().keys())
