3a6c8c5c9091e29e027e0821f670252bec56a1ba
Jeny Sadadia <jeny.sadadia@collabora.com>
2023-10-18 15:30:42 +0530

    api.main: renamed versioned app instance
    At the moment, original `FastAPI` app instance and versioned
    app instance both has same name `app`.
    Renamed the versioned app instance in order to use original
    app instace for mocking dependency callable with
    `app.dependency_overrides`. This is required to mock calls
    to `get_current_user` and `get_current_superuser` for user
    related tests.
    Update `Dockerfile` to use `versioned_app` instance
    for running API.
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

--- api/main.py
+++ api/main.py
@@ -626,7 +626,7 @@ async def put_regression(regression_id: str, regression: Regression,
     return obj
 
 
-app = VersionedFastAPI(
+versioned_app = VersionedFastAPI(
         app,
         version_format='{major}',
         prefix_format='/v{major}',
@@ -645,7 +645,7 @@ app = VersionedFastAPI(
 The issue has already been reported here:
 https://github.com/DeanWay/fastapi-versioning/issues/30
 """
-for sub_app in app.routes:
+for sub_app in versioned_app.routes:
     if hasattr(sub_app.app, "add_exception_handler"):
         sub_app.app.add_exception_handler(
             ValueError, value_error_exception_handler
