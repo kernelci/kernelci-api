6280278ca7b1f86f4c284a2f4b68a7632a965563
Jeny Sadadia <jeny.sadadia@collabora.com>
2024-03-06 18:06:40 +0530

    api.main: fix `pylint` error
    Fix the below pylint error:
    ```
    api/main.py:741:4: W0621: Redefining name 'root' from outer scope (line 112) (redefined-outer-name)
    ```
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

--- api/main.py
+++ api/main.py
@@ -738,8 +738,8 @@ async def viewer():
     """Serve simple HTML page to view the API /static/viewer.html
     Set various no-cache tag we might update it often"""
 
-    root = os.path.dirname(os.path.abspath(__file__))
-    viewer_path = os.path.join(root, 'templates', 'viewer.html')
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    viewer_path = os.path.join(root_dir, 'templates', 'viewer.html')
     with open(viewer_path, 'r', encoding='utf-8') as file:
         # set header to text/html and no-cache stuff
         hdr = {
