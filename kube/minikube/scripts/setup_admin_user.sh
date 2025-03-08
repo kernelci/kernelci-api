6a811a932d20a4009245a7b26c0a8fc1f8a5c43a
Staging script <kernelci@kernelci.org>
2025-03-08 16:03:11 +0000

    Staging PR 599

--- kube/minikube/scripts/setup_admin_user.sh
+++
@@ -1,2 +0,0 @@
-API_POD_NAME=$(kubectl get ep kernelci-api -o=jsonpath='{.subsets[*].addresses[*].ip}' | tr ' ' '\n' | xargs -I % kubectl get pods -o=name --field-selector=status.podIP=%)
-kubectl exec -it $API_POD_NAME -- python3 -m api.admin --mongo mongodb://kernelci-api-db.default.svc.cluster.local:27017 --email bot@kernelci.org
