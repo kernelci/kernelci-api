b8c0b88c594eae45e4ff16a6021301834ba11183
Denys Fedoryshchenko <denys.f@collabora.com>
2025-05-30 10:24:24 +0300

    Add myself to authors and update copyright line
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>

--- api/main.py
+++ api/main.py
@@ -1,8 +1,9 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 #
-# Copyright (C) 2021-2023 Collabora Limited
+# Copyright (C) 2021-2025 Collabora Limited
 # Author: Guillaume Tucker <guillaume.tucker@collabora.com>
 # Author: Jeny Sadadia <jeny.sadadia@collabora.com>
+# Author: Denys Fedoryshchenko <denys.f@collabora.com>
 
 # pylint: disable=unused-argument,global-statement,too-many-lines
 
